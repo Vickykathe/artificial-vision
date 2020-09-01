65082c058021771cb6e9db8a001b774ae4190763
vickykathe <kathevela30@hotmail.com>
2020-09-01 17:51:01 -0500

    Ejemplos pyton

---
+++ basic_calc1.py
@@ -0,0 +1,13 @@
+#Developer: Kathe Vela
+#Script: this program let you add two static numbers
+
+#clear screen class
+import os
+
+os.system("cls")
+
+x = 20
+y = 30
+
+add = x + y
+print("The add is: ", add)
---
+++ myAge.py
@@ -0,0 +1,14 @@
+
+#Developer: Kathe Vela
+'''
+What does this script do?:
+This script show your name and your age.
+'''
+
+myName = 'Kvela'
+myDate = 1989
+year = 2020
+
+age = year - myDate
+
+print ("Your name is: ", myName, "and you are ", age, "years old")
